ac5bc550ae83a30937e4d2c735cbc86ad9e94b21
Valentinas Straigis <val.straigis@gmail.com>
2024-04-08 00:00:06 +0300

    refactor(home): remove redundant code, add aws4 module for mongodb

--- app/page.tsx
+++ app/page.tsx
@@ -1,70 +1,23 @@
 "use client";
-import { connectToDatabase } from "@/app/utils/database";
-import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
-import Link from "next/link";
-import { useEffect, useState } from 'react'
-
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-
-  const [providers, setProviders] = useState<any | null>(null);
   const { data: session } = useSession();
 
-  useEffect(() => {
-    const setUpProviders = async () => {
-      const response = await getProviders()
-      setProviders(response)
-    }
-    setUpProviders();
-
-  }, [])
-
-  //connectToDatabase();
   return (
     <section className="">
-
       <div className="flex flex-col items-center justify-center w-full h-full">
-        {session ? <h1 className="text-3xl">Authorized, {session.user?.name}!</h1> : <h1 className="text-3xl">Not Authorized</h1>}
-        <h1 className="text-3xl">Tai tik gariunai internetu</h1>
-        <span className="text-xs font-light">for legal reasons this is a joke</span>
-
-      </div>
-<div>
-      {session?.user ?
-        (
-          <div className='flex gap-3 md:gap-5'>
-            
-            <button type='button' onClick={() => signOut()} className='outline_btn'>
-              Sign Out
-            </button>
-
-            <Link href='/profile'>
-
-            </Link>
-          </div>
+        {session ? (
+          <h1 className="text-3xl">Authorized, {session.user?.name}!</h1>
         ) : (
-          <>
-            {
-              providers &&
-              Object.values(providers).map((provider: any) => (
-                <button type='button'
-                  key={provider.name}
-                  onClick={() => signIn(provider.id)}
-                  className='black_btn'>
-                  Sign in
-                </button>
-              ))
-            }
-          </>
+          <h1 className="text-3xl">Not Authorized</h1>
         )}
-    </div>
-
-
-      
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-
+        <h1 className="text-3xl">Tai tik gariunai internetu</h1>
+        <span className="text-xs font-light">
+          for legal reasons this is a joke
+        </span>
       </div>
-    </section >
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div>
+    </section>
   );
 }
