94df09358497c602397754bb5231c799ba9757e3
Dominykas3000 <dominykas.svilpa@mif.stud.vu.lt>
2024-04-08 19:11:46 +0300

    for pulling

--- models/user.ts
+++ models/user.ts
@@ -10,6 +10,7 @@ const UserSchema = new Schema({
       "Username invalid, it should contain 5-20 alphanumeric letters and be unique!",
     ],
   },
+  
    password: {
      type: String,
      required: [true, "Password is required!"],
@@ -17,17 +18,20 @@ const UserSchema = new Schema({
        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
        "Password invalid, it should contain at least 8 characters, one uppercase letter, one lowercase letter and one number!",
      ],
-   },
+  },
+   
   email: {
     type: String,
     unique: [true, "Email already exists!"],
     required: [true, "Email is required!"],
   },
+  
   privileges: {
     type: String,
     enum: ["admin", "user"],
     default: "user",
   },
+
   role: {
     type: String,
     enum: ["buyer", "seller"],
