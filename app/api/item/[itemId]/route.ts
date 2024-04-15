a13e8cfa9e9e6567fe0ebb979225cc92714f6c9f
Valentinas S <97912354+vstraigis@users.noreply.github.com>
2024-04-15 17:58:32 +0300

    Seller crud sale price (#14)
    * CRUD not working :(
    
    * valentinai atleisk, kad neleidziu miegot
    
    * fix(itemfetch): fix item fetch
    
    * finished CRUD, need to fix images, saleStartDate/end, saleactive and frontend
    
    ---------
    
    Co-authored-by: arnasvid <arnas.vidziunas@gmail.com>

--- app/api/item/[itemId]/route.ts
+++ app/api/item/[itemId]/route.ts
@@ -1,23 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
-
+import Item from "@/models/item";
+import { connectToDatabase } from "@/utils/database";
 
 export async function GET(
-  req: NextRequest, { params }: { params: { itemId: string } }
-) {
-  
-  return NextResponse.json({
-    status: true,
-    message: `Return product: ${params.itemId} route`,
-  });
-}
-export async function POST(
-  request: NextRequest,
+  req: NextRequest,
   { params }: { params: { itemId: string } }
 ) {
 
+  await connectToDatabase();
+  const item = await Item.findById(params.itemId);
+
   return NextResponse.json({
     status: true,
-    message: `Create product: ${params.itemId} route`,
+    item: item.toJSON(),
   });
 }
 
@@ -25,6 +20,42 @@ export async function PUT(
   req: NextRequest,
   { params }: { params: { itemId: string } }
 ) {
+  const {
+    newName: name,
+    newPrice: price,
+    newDescription: description,
+    newTags: tags,
+    newStock: stock,
+    image,
+    salePrice,
+    saleStartDate,
+    saleEndDate,
+    saleActive,
+    newCategory: category,
+    sellerId,
+  } = await req.json();
+  await connectToDatabase();
+
+  // TODO: Implement saleActive logic
+  // if( (salePrice < price) && saleStartDate <= Date() && saleEndDate >= Date() ) {
+  //   const saleActive = true;
+  // }
+
+  await Item.findByIdAndUpdate(params.itemId, {
+    name,
+    price,
+    description,
+    tags,
+    stock,
+    image,
+    salePrice,
+    saleStartDate,
+    saleEndDate,
+    saleActive,
+    category,
+    sellerId,
+  });
+
   return NextResponse.json({
     status: true,
     message: `Update product: ${params.itemId} route`,
@@ -35,6 +66,10 @@ export async function DELETE(
   req: NextRequest,
   { params }: { params: { itemId: string } }
 ) {
+  const id = params.itemId;
+  await connectToDatabase();
+  await Item.findByIdAndDelete(id);
+
   return NextResponse.json({
     status: true,
     message: `Delete product: ${params.itemId} route`,
---
+++ components/EditItemForm.tsx
@@ -0,0 +1,159 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const EditItemForm = ({
+  id,
+  name,
+  price,
+  description,
+  tags,
+  stock,
+  category,
+}: {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  tags: string[];
+  stock: number;
+  category: string;
+}) => {
+  const [newName, setNewName] = useState(name);
+  const [newPrice, setNewPrice] = useState(price);
+  const [newDescription, setNewDescription] = useState(description);
+  const [newTags, setNewTags] = useState(tags);
+  const [newStock, setNewStock] = useState(stock);
+  const [newCategory, setNewCategory] = useState(category);
+  const [newImage, setNewImage] = useState("");
+  const [salePrice, setSalePrice] = useState(0);
+  const [newStartDate, setNewStartDate] = useState("");
+  const [newEndDate, setNewEndDate] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(`http://localhost:3000/api/item/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          newName,
+          newPrice,
+          newDescription,
+          newTags,
+          newStock,
+          newImage,
+          salePrice,
+          newStartDate,
+          newEndDate,
+          newCategory,
+        }),
+      });
+
+      console.log(salePrice);
+      if (!res.ok) {
+        throw new Error("Failed to update topic");
+      }
+
+      router.refresh();
+      router.push("/dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+      <input
+        onChange={(e) => setNewName(e.target.value)}
+        value={newName}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Item Name"
+      />
+      <input
+        onChange={(e) => setNewPrice(Number(e.target.value))}
+        value={newPrice}
+        className="border border-slate-500 px-8 py-2"
+        type="Number"
+        placeholder="Item Price (in Eur)"
+      />
+      <input
+        onChange={(e) => setNewDescription(e.target.value)}
+        value={newDescription}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Item Description"
+      />
+      <input
+        onChange={(e) => setNewTags([e.target.value])}
+        value={newTags}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Tags"
+      />
+      <input
+        onChange={(e) => setNewStock(Number(e.target.value))}
+        value={newStock}
+        className="border border-slate-500 px-8 py-2"
+        type="number"
+        placeholder="Stock"
+      />
+      <input
+        onChange={(e) => setNewCategory(e.target.value)}
+        value={newCategory}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Category"
+      />
+      <input
+        onChange={(e) => setNewImage(e.target.value)}
+        value={newImage}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Image URL"
+      />
+      <input
+        onChange={(e) => setSalePrice(Number(e.target.value))}
+        value={salePrice}
+        className="border border-slate-500 px-8 py-2"
+        type="number"
+        placeholder="Sale Price"
+      />
+      <div>
+        <label className="text-gray-900">Sale Start Date</label>
+        <input
+          onChange={(e) => setNewStartDate(e.target.value)}
+          value={newStartDate}
+          className="border border-slate-500 px-8 py-2"
+          type="date"
+          placeholder="Sale Start Date"
+        />
+      </div>
+      <div>
+        <label className="text-gray-900">Sale End Date</label>
+        <input
+          onChange={(e) => setNewEndDate(e.target.value)}
+          value={newEndDate}
+          className="border border-slate-500 px-8 py-2"
+          type="date"
+          placeholder="Sale End Date"
+        />
+      </div>
+      <button
+        type="submit"
+        className="w-full text-white bg-gray-900 hover:bg-gray-800 font-medium rounded-lg text-base px-5 py-2.5 text-center dark:gray-900 dark:hover:bg-gray-800 dark:focus:ring-gray-800"
+      >
+        Update Item
+      </button>
+    </form>
+  );
+};
+
+export default EditItemForm;
